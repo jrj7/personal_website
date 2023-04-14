09cca3408d9f84600248aefeb1c90ea61bacd07d
Jessica R. Jones <jones.jessr@gmail.com>
2023-04-14 11:47:41 -0400

    Final bootstrap changes
    Completed rework to bootstrap5
    adjustments to scripts

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 // handle links with @href started with '#' only
 $(document).on('click', 'a[href^="#"]', function(e) {
+    e.preventDefault()
+
     // target element id
     var id = $(this).attr('href');
 
@@ -8,13 +10,16 @@ $(document).on('click', 'a[href^="#"]', function(e) {
     if ($id.length === 0) {
         return;
     }
+    
+    //scroll to top
+    if(id == '#title'){
+        $('body, html').animate({scrollTop: 0});
+    }
+    else {
+        //scroll to element
+        var pos = $id.offset().top;
 
-    // prevent standard hash navigation (avoid blinking in IE)
-    e.preventDefault();
-
-    // top position relative to the document
-    var pos = $id.offset().top;
-
-    // animated top scrolling
-    $('body, html').animate({scrollTop: pos});
+        // animated top scrolling
+        $('body, html').animate({scrollTop: pos});
+    }  
 });
