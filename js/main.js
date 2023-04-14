6e93df5c04a2628757151fd046cbdd6fc5505669
Jessica R. Jones <jones.jessr@gmail.com>
2023-04-14 12:16:06 -0400

    Removed unnecessary files
    Removed files no longer using
    Formatted HTML/CSS/JS files

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
 // handle links with @href started with '#' only
-$(document).on('click', 'a[href^="#"]', function(e) {
-    e.preventDefault()
+$(document).on("click", 'a[href^="#"]', function (e) {
+    e.preventDefault();
 
     // target element id
-    var id = $(this).attr('href');
+    var id = $(this).attr("href");
 
     // target element
     var $id = $(id);
     if ($id.length === 0) {
         return;
     }
-    
+
     //scroll to top
-    if(id == '#title'){
-        $('body, html').animate({scrollTop: 0});
-    }
-    else {
+    if (id == "#title") {
+        $("body, html").animate({ scrollTop: 0 });
+    } else {
         //scroll to element
         var pos = $id.offset().top;
 
         // animated top scrolling
-        $('body, html').animate({scrollTop: pos});
-    }  
+        $("body, html").animate({ scrollTop: pos });
+    }
 });
